3b167f617513e1153c231cfd8aac61a0f5d2fd7f
obscure <sheikharifinae4@gmail.com>
2026-09-02 22:32:09 +0600

    Update update.py

--- update.py
+++ update.py
@@ -1,15 +1,9 @@
 import os
-import subprocess
 
-UPSTREAM_REPO = os.environ.get('UPSTREAM_REPO', 'https://github.com/obscure-n8/ZxZone-Master-MLTB')
-UPSTREAM_BRANCH = os.environ.get('UPSTREAM_BRANCH', 'main')
+def update_repo():
+    os.system("git fetch --all")
+    os.system("git reset --hard origin/main")
+    os.system("pip install --upgrade -r requirements.txt")
 
 if __name__ == "__main__":
-    if os.path.exists('.git'):
-        try:
-            subprocess.run(["git", "fetch", "origin", UPSTREAM_BRANCH], check=True)
-            subprocess.run(["git", "reset", "--hard", f"origin/{UPSTREAM_BRANCH}"], check=True)
-        except Exception:
-            pass
-            
-    subprocess.run(["pip3", "install", "--no-cache-dir", "-r", "requirements.txt"])
+    update_repo()
