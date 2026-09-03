1e3b2cc73a5041e1f81fb65df859a8a4c943eeb3
obscure <sheikharifinae4@gmail.com>
2026-09-03 20:18:16 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -e
 
-# Sync/update config before starting (if you have this step)
-python3 update.py
-
-# Start alive server in background (keeps port bound for Heroku)
 python3 alive.py &
 
-# Run the bot as a package (NOT bot.py, since bot/ is a folder)
 exec python3 -m bot
