8b47b97578f6bcd815f85f4ad0a5bc44264afa1b
obscure <sheikharifinae4@gmail.com>
2026-09-03 20:14:29 +0600

    Update alive.py

--- alive.py
+++ alive.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +8,5 @@ def home():
     return "ZxZone-Master-MLTB is alive ✅"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
